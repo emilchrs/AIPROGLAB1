a2202c78e0996e708ad1574036c4e740e2cbe8ed
Emil <emilkabusa@hotmail.com>
2021-09-09 08:41:22 +0200

    added 'other tile' in minimax

--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -7,3 +7,26 @@ module Minimax =
     type Class1() = 
         member this.X = "F#"
     
+//OtherTile
+
+type OtherTile = 
+    | Black
+    | White
+    member this.opposite =
+        match this with
+        | Black -> White
+        | White -> Black
+//GetWinner
+
+//CountCorners
+
+//Evaluation
+
+//GetValidMoves
+
+//MakeMove
+
+
+
+//Minimax
+
